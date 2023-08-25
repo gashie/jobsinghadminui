7428890af3835b414b39a939aaaebfe2c7a16759
ingucell <mathiaslawson70@gmail.com>
2023-08-25 10:24:27 +0000

    home design check

--- src/pages/Pages/Front/Home/FeaturedJobs.js
+++ src/pages/Pages/Front/Home/FeaturedJobs.js
@@ -1,5 +1,17 @@
-import React, {useState} from "react";
-import { Row, Col, Card, CardBody, Button, Container, NavLink, NavItem, TabPane, TabContent, Nav } from "reactstrap";
+import React, { useState } from "react";
+import {
+  Row,
+  Col,
+  Card,
+  CardBody,
+  Button,
+  Container,
+  NavLink,
+  NavItem,
+  TabPane,
+  TabContent,
+  Nav,
+} from "reactstrap";
 import { Link } from "react-router-dom";
 import { jobGrid } from "../../../../common/data/appsJobs";
 import classnames from "classnames";
@@ -32,58 +44,79 @@ const FeaturedJobs = () => {
   ];
 
   const [arrowNavTab, setarrowNavTab] = useState("1");
-    const arrowNavToggle = (tab) => {
-        if (arrowNavTab !== tab) {
-            setarrowNavTab(tab);
-        }
-    };
+  const arrowNavToggle = (tab) => {
+    if (arrowNavTab !== tab) {
+      setarrowNavTab(tab);
+    }
+  };
 
-    const [customActiveTab, setcustomActiveTab] = useState("1");
-    const toggleCustom = (tab) => {
-        if (customActiveTab !== tab) {
-            setcustomActiveTab(tab);
-        }
-    };
+  const [customActiveTab, setcustomActiveTab] = useState("1");
+  const toggleCustom = (tab) => {
+    if (customActiveTab !== tab) {
+      setcustomActiveTab(tab);
+    }
+  };
 
   return (
     <>
-      <Container fluid className="col-md-10 col-xxl-10">
-        <Row>
-          <div
-            style={{ display: "flex", justifyContent: "space-between", marginTop: '40rem' }}
-            className=" p-4"
-          >
-            <h4>Featured Jobs</h4>
-            <Button style={{ backgroundColor: "#244a59" }}>See all jobs</Button>
-          </div>
-        </Row>
-        <Row id="job-list" className="p-3">
-          {jobGrid.map((item, key) => (
-            <Col xl={3} md={6} key={key} xs={15} >
-              <Card className="">
-                <CardBody>
-                  <Button
-                    type="button"
-                    className="btn btn-icon btn-soft-primary float-end"
-                    data-bs-toggle="button"
-                    aria-pressed="true"
-                  >
-                    <i className="mdi mdi-cards-heart fs-16"></i>
-                  </Button>
-                  <div className="avatar-md mb-4">
-                    <div className="">
-                      <img
-                        src={item.companyLogo}
-                        alt=""
-                        className="avatar-xxl"
-                      />
+      <div style={{ backgroundColor: "white" }}>
+        <Container fluid className="col-md-10 col-xxl-10">
+          <Row>
+            <div
+              style={{
+                display: "flex",
+                justifyContent: "space-between",
+                marginTop: "3rem",
+              }}
+              className=" p-4"
+            >
+              <h4 style={{ color: "244a59" }} className="fw-bolder">
+                Featured Jobs
+              </h4>
+              <Button
+                style={{ backgroundColor: "#244a59" }}
+                className="btn btn-dark"
+              >
+                See all jobs
+              </Button>
+            </div>
+          </Row>
+          <Row id="job-list" className="p-3">
+            {jobGrid.map((item, key) => (
+              <Col xl={3} md={6} key={key} xs={15}>
+                <Link to='/job-details'>
+                <Card
+                  className=""
+                  style={{
+                    boxShadow: "none",
+                    border: "1px solid #e0e0e0",
+                    cursor: "pointer",
+                  }}
+                >
+                  <CardBody>
+                    <Button
+                      type="button"
+                      className="btn btn-dark btn-soft-primary float-end"
+                      style={{ backgroundColor: "white", boxShadow: "none" }}
+                    >
+                      <i className="mdi mdi-cards-heart fs-16"></i>
+                    </Button>
+                    <div className="avatar-md mb-4">
+                      <div className="">
+                        <img
+                          src={item.companyLogo}
+                          alt=""
+                          className="avatar-xxl"
+                        />
+                      </div>
                     </div>
-                  </div>
-                  <Link to="#!">
-                    <h5>{item.jobTitle}</h5>
-                  </Link>
-                  {/* <p className="text-muted">{item.companyName} </p> */}
-                  {/* <div className="d-flex gap-4 mb-3">
+                    <Link to="#!">
+                      <h5 className="fw-bolder" style={{ color: "#244a59" }}>
+                        {item.jobTitle}
+                      </h5>
+                    </Link>
+                    {/* <p className="text-muted">{item.companyName} </p> */}
+                    {/* <div className="d-flex gap-4 mb-3">
                       <div>
                         <i className="ri-map-pin-2-line text-primary me-1 align-bottom"></i>{" "}
                         {item.location}
@@ -93,18 +126,18 @@ const FeaturedJobs = () => {
                         {item.postDate}
                       </div>
                     </div> */}
-                  <p className="text-muted mt-5">{item.description}</p>
-                  <p className="text-muted">
-                    {" "}
-                    <i className="ri-map-pin-2-line text-primary me-1 align-bottom"></i>{" "}
-                    {item.location}
-                  </p>
-                  <p className="text-muted">
-                    {" "}
-                    <i className="ri-map-pin-2-line text-primary me-1 align-bottom"></i>{" "}
-                    {item.postDate}
-                  </p>
-                  {/* <div className="hstack gap-2">
+                    <p className="text-muted mt-5">{item.description}</p>
+                    <p className="text-muted mt-5">
+                      {" "}
+                      <i className="ri-map-pin-2-line text-muted me-1 align-bottom fs-20"></i>{" "}
+                      Accra
+                    </p>
+                    <p className="text-muted mt-4">
+                      {" "}
+                      <i className="bx bx-layer text-muted me-1 align-bottom fs-20"></i>{" "}
+                      Expires Jul 25, 2023
+                    </p>
+                    {/* <div className="hstack gap-2">
                       {item.requirement.map((subitem, key) => (
                         <React.Fragment key={key}>
                           {subitem === "Full Time" ? (
@@ -123,7 +156,7 @@ const FeaturedJobs = () => {
                         </React.Fragment>
                       ))}
                     </div> */}
-                  {/* <div className="mt-4 hstack gap-2">
+                    {/* <div className="mt-4 hstack gap-2">
                       <Link to="#!" className="btn btn-soft-primary w-100">
                         Apply Job
                       </Link>
@@ -134,162 +167,169 @@ const FeaturedJobs = () => {
                         Overview
                       </Link>
                     </div> */}
-                </CardBody>
-              </Card>
+                  </CardBody>
+                </Card>
+                </Link>
+              </Col>
+            ))}
+          </Row>
+
+          {/* Browse Jobs */}
+          <Row>
+            <Col xxl={6}>
+              <h5
+                className="mb-3 mt-5 p-4"
+                style={{
+                  fontWeight: "bolder",
+                  marginTop: "10rem",
+                }}
+              >
+                Browse Jobs
+              </h5>
+              <Nav
+                tabs
+                className="nav nav-tabs nav-tabs-custom nav-justified mb-3 p-4"
+              >
+                <NavItem>
+                  <NavLink
+                    style={{ cursor: "pointer", padding: "" }}
+                    className={classnames({
+                      active: customActiveTab === "1",
+                    })}
+                    onClick={() => {
+                      toggleCustom("1");
+                    }}
+                  >
+                    By Categories
+                  </NavLink>
+                </NavItem>
+                <NavItem>
+                  <NavLink
+                    style={{ cursor: "pointer" }}
+                    className={classnames({
+                      active: customActiveTab === "2",
+                    })}
+                    onClick={() => {
+                      toggleCustom("2");
+                    }}
+                  >
+                    By Industry
+                  </NavLink>
+                </NavItem>
+              </Nav>
+
+              <TabContent activeTab={customActiveTab} className="text-muted">
+                <TabPane tabId="1" id="home1">
+                  <div className="d-flex">
+                    <div
+                      className="ms-2 p-3"
+                      style={{
+                        display: "flex",
+                        gap: "2rem",
+                        flexWrap: "wrap",
+                      }}
+                    >
+                      <Col>
+                        <p>Accounting</p>
+                        <p>Data Management</p>
+                        <p>Extractive</p>
+                        <p>Health and Nutrition</p>
+                        <p>Insurance</p>
+                        <p>Automotive/Machinery/Aviation</p>
+                        <p>Banking</p>
+                        <p>Policy</p>
+                        <p>Scince</p>
+                      </Col>
+                      <Col>
+                        <p>Accounting</p>
+                        <p>Agriculture</p>
+                        <p>Driving/Transportation</p>
+                        <p>Health and Nutrition</p>
+                        <p>Insurance</p>
+                        <p>Automotive/Machinery/Aviation</p>
+                        <p>Banking</p>
+                        <p>Policy</p>
+                        <p>Scince</p>
+                      </Col>
+                      <Col>
+                        <p>Accounting</p>
+                        <p>Data Management</p>
+                        <p>Extractive</p>
+                        <p>Health and Nutrition</p>
+                        <p>Insurance</p>
+                        <p>Automotive/Machinery/Aviation</p>
+                        <p>Banking</p>
+                        <p>Policy</p>
+                        <p>Scince</p>
+                      </Col>
+                    </div>
+                  </div>
+                </TabPane>
+                <TabPane tabId="2">
+                  <div className="d-flex">
+                    <div
+                      className="ms-2 p-3"
+                      style={{
+                        display: "flex",
+                        gap: "1rem",
+                        flexWrap: "wrap",
+                      }}
+                    >
+                      <Col>
+                        <p>Administration</p>
+                        <p>Agriculture</p>
+                        <p>Driving/Transportation</p>
+                        <p>Health and Nutrition</p>
+                        <p>Insurance</p>
+                        <p>Automotive/Machinery/Aviation</p>
+                        <p>Banking</p>
+                        <p>Policy</p>
+                        <p>Scince</p>
+                      </Col>
+                      <Col>
+                        <p>Administration</p>
+                        <p>Agriculture</p>
+                        <p>Driving/Transportation</p>
+                        <p>Health and Nutrition</p>
+                        <p>Insurance</p>
+                        <p>Automotive/Machinery/Aviation</p>
+                        <p>Banking</p>
+                        <p>Policy</p>
+                        <p>Scince</p>
+                      </Col>
+                      <Col>
+                        <p>Administration</p>
+                        <p>Agriculture</p>
+                        <p>Driving/Transportation</p>
+                        <p>Health and Nutrition</p>
+                        <p>Insurance</p>
+                        <p>Automotive/Machinery/Aviation</p>
+                        <p>Banking</p>
+                        <p>Policy</p>
+                        <p>Scince</p>
+                      </Col>
+                      <Col>
+                        <p>Accounting</p>
+                        <p>Agriculture</p>
+                        <p>Driving/Transportation</p>
+                        <p>Health and Nutrition</p>
+                        <p>Insurance</p>
+                        <p>Automotive/Machinery/Aviation</p>
+                        <p>Banking</p>
+                        <p>Policy</p>
+                        <p>Scince</p>
+                      </Col>
+                    </div>
+                  </div>
+                </TabPane>
+              </TabContent>
             </Col>
-          ))}
-        </Row>
-
-        {/* Browse Jobs */}
-        <Row>
-          <Col xxl={6}>
-            <h5 className="mb-3 mt-5 p-4" style={{
-                fontWeight: 'bolder', 
-                marginTop: '10rem'
-            }}>Browse Jobs</h5>           
-                <Nav
-                  tabs
-                  className="nav nav-tabs nav-tabs-custom nav-justified mb-3 p-4"
-                >
-                  <NavItem>
-                    <NavLink
-                      style={{ cursor: "pointer", padding: '' }}
-                      className={classnames({
-                        active: customActiveTab === "1",
-                      })}
-                      onClick={() => {
-                        toggleCustom("1");
-                      }}
-                    >
-                      By Categories
-                    </NavLink>
-                  </NavItem>
-                  <NavItem>
-                    <NavLink
-                      style={{ cursor: "pointer" }}
-                      className={classnames({
-                        active: customActiveTab === "2",
-                      })}
-                      onClick={() => {
-                        toggleCustom("2");
-                      }}
-                    >
-                      By Industry
-                    </NavLink>
-                  </NavItem>
-                </Nav>
-
-                <TabContent activeTab={customActiveTab} className="text-muted">
-                  <TabPane tabId="1" id="home1">
-                    <div className="d-flex">
-                     
-                      <div className="ms-2 p-3" style={{
-                        display: "flex", gap: '2rem' , flexWrap: "wrap"
-                      }}>
-                        <Col>
-                        <p>Accounting</p>      
-                        <p>Data Management</p>  
-                        <p>Extractive</p>  
-                        <p>Health and Nutrition</p>  
-                        <p>Insurance</p>  
-                        <p>Automotive/Machinery/Aviation</p>  
-                        <p>Banking</p>  
-                        <p>Policy</p>  
-                        <p>Scince</p>  
-                        </Col>
-                        <Col>
-                        <p>Accounting</p>      
-                        <p>Agriculture</p>  
-                        <p>Driving/Transportation</p>  
-                        <p>Health and Nutrition</p>  
-                        <p>Insurance</p>  
-                        <p>Automotive/Machinery/Aviation</p>  
-                        <p>Banking</p>  
-                        <p>Policy</p>  
-                        <p>Scince</p>  
-                        </Col>
-                        <Col>
-                        <p>Accounting</p>      
-                        <p>Data Management</p>  
-                        <p>Extractive</p>  
-                        <p>Health and Nutrition</p>  
-                        <p>Insurance</p>  
-                        <p>Automotive/Machinery/Aviation</p>  
-                        <p>Banking</p>  
-                        <p>Policy</p>  
-                        <p>Scince</p>  
-                        </Col>
-                       
-                      
-                      </div>
-                    </div>
-                  </TabPane>
-                  <TabPane tabId="2" >
-                    <div className="d-flex">
-                     
-                      <div className="ms-2 p-3" style={{
-                        display: "flex", gap: '1rem' , flexWrap: "wrap"
-                      }}>
-                        <Col>
-                        <p>Administration</p>      
-                        <p>Agriculture</p>  
-                        <p>Driving/Transportation</p>  
-                        <p>Health and Nutrition</p>  
-                        <p>Insurance</p>  
-                        <p>Automotive/Machinery/Aviation</p>  
-                        <p>Banking</p>  
-                        <p>Policy</p>  
-                        <p>Scince</p>  
-                        </Col>
-                        <Col>
-                        <p>Administration</p>      
-                        <p>Agriculture</p>  
-                        <p>Driving/Transportation</p>  
-                        <p>Health and Nutrition</p>  
-                        <p>Insurance</p>  
-                        <p>Automotive/Machinery/Aviation</p>  
-                        <p>Banking</p>  
-                        <p>Policy</p>  
-                        <p>Scince</p>  
-                        </Col>
-                        <Col>
-                        <p>Administration</p>      
-                        <p>Agriculture</p>  
-                        <p>Driving/Transportation</p>  
-                        <p>Health and Nutrition</p>  
-                        <p>Insurance</p>  
-                        <p>Automotive/Machinery/Aviation</p>  
-                        <p>Banking</p>  
-                        <p>Policy</p>  
-                        <p>Scince</p>  
-                        </Col>
-                        <Col>
-                        <p>Accounting</p>      
-                        <p>Agriculture</p>  
-                        <p>Driving/Transportation</p>  
-                        <p>Health and Nutrition</p>  
-                        <p>Insurance</p>  
-                        <p>Automotive/Machinery/Aviation</p>  
-                        <p>Banking</p>  
-                        <p>Policy</p>  
-                        <p>Scince</p>  
-                        </Col>
-                      
-                      </div>
-                    </div>
-                  </TabPane>
-                 
-                 
-                </TabContent>
-             
-          </Col>
-
-       
-        </Row>
-        <br />
-        <br />
-        <br />
-      </Container>
+          </Row>
+          <br />
+          <br />
+          <br />
+        </Container>
+      </div>
     </>
   );
 };
--- src/pages/Pages/Front/Home/Search.js
+++ src/pages/Pages/Front/Home/Search.js
@@ -1,76 +1,67 @@
-import {Row, Col, Input} from 'reactstrap'
-import bg from '../../../../assets/images/jobsinghana/bg2.png'
-import './Home.css'
+import { Row, Col, Input } from "reactstrap";
+import bg from "../../../../assets/images/jobsinghana/bg2.png";
+import "./Home.css";
 const Search = () => {
-   return (
+  return (
     <>
-        
-              <div className="col-xxl-15 col-md-14 text-bg-color">
-                <div className="card crm-widget">
-                  <div className="card-body p-0 text-bg"
-                  > 
-                    <div
-                      className="row row-cols-xxl-5  row-cols-xxs-10 row-cols-md-3 row-cols-1 g-0 p-5"
-                      style={{
-                       
-                        display: "flex",
-                        justifyContent: "center",
-                        height: '55vh',
-                        
-                      }}
-                    >
-                        
-                      <div
-                        className="col-auto mt-5"
-                        style={{
-                          backgroundColor: "white",
-                          padding: "2rem",
-                          borderRadius: "0.7rem",
-                          display: "flex",
-                          justifyContent: "space-between",
-                          gap: "1rem",
-                          width: "100rem",
-                          alignContent: "center",
-                          height: '20vh', 
-                         
-                        }}
-                      >
-                         
-                        <div className="col-md-10">
-                          <h6 style={{fontWeight: 'bolder'}}>Search for your new job</h6>
-                          <Input
-                            type="text"
-                            className="form-control form-control-lg bg-light border-light"
-                            placeholder="Search for jobs of companies"
-                          />
-                        </div>
-                        
-                        <div className="col-md-2">
-                          <button
-                            type="submit"
-                            className="btn btn-primary btn-lg waves-effect waves-light"
-                            style={{
-                              backgroundColor: "#244A59",
-                              fontSize: "0.8rem",
-                              marginTop: '1.7rem',
-                              width: 'max-content'
-                            }}
-                          >
-                            {" "}
-                            Find a Job
-                          </button>
-                        </div>
+      <div className="col-xxl-15 col-md-14 text-bg-color" style={{backgroundColor: 'white'}}>
+        <div className="card crm-widget">
+          <div className="card-body p-0 search-bg">
+            <div
+              className="row row-cols-xxl-5  row-cols-xxs-10 row-cols-md-3 row-cols-1 g-0 p-5"
+              style={{
+                display: "flex",
+                justifyContent: "center",
+                height: "55vh",
+              }}
+            >
+              <div
+                className="col-auto mt-5"
+                style={{
+                  backgroundColor: "white",
+                  padding: "2rem",
+                  borderRadius: "0.7rem",
+                  display: "flex",
+                  justifyContent: "space-between",
+                  gap: "1rem",
+                  width: "100rem",
+                  alignContent: "center",
+                  height: "20vh",
+                }}
+              >
+                <div className="col-md-10" style={{ width: "65%" }}>
+                  <h6 style={{ fontWeight: "bolder"}}>
+                    Search for your new job
+                  </h6>
+                  <Input
+                    type="text"
+                    className="form-control form-control-lg bg-light border-light"
+                    placeholder="Search for jobs of companies"
+                  />
+                </div>
 
-                        
-                      </div>
-                    </div>
-                  </div>
+                <div className="col-md-2 col-xs-20" style={{width: "35%"}}>
+                  <button
+                    type="submit"
+                    className="btn btn-dark waves-effect waves-light"
+                    style={{
+                      backgroundColor: "#244A59",
+                      fontSize: "0.8rem",
+                      marginTop: "1.7rem",
+                      width: '100%'
+                    }}
+                  >
+                    {" "}
+                    Find a Job
+                  </button>
                 </div>
               </div>
-          
-    
+            </div>
+          </div>
+        </div>
+      </div>
     </>
-   )
-}
+  );
+};
 
 export default Search;
